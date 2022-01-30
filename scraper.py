557b74ab898a65ca9eca27d74d1917d99240229d
Adrian Bjugård <adrian@bjugard.io>
2022-01-30 12:34:08 +0100

    Fix deprecation message

--- scraper.py
+++ scraper.py
@@ -150,4 +150,4 @@ async def main():
 if __name__ == '__main__':
   config = load_config()
 
-  asyncio.get_event_loop().run_until_complete(main())
+  asyncio.new_event_loop().run_until_complete(main())
